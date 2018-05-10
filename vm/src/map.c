bdc7b0f9bdc8097976198fa07938daf94ccbb0c2
Vincent LAY <vlay@e1r2p6.42.fr>
2018-05-10 15:32:13 +0200

    Fixed unnecessary bug.

--- vm/src/map.c
+++ vm/src/map.c
@@ -6,18 +6,21 @@ void	setmap(t_map *map, t_champ *champs, t_list *allprocess)
 	size_t	i;
 	size_t	c;
 	size_t	interval;
+	t_process	*process;
 
 	interval = MEM_SIZE / champslen(champs);
-	i = 0;
-	while (i < champslen(champs))
+	i = champslen(champs);
+	while (i)
 	{
+		i--;
 		ft_memmove(&map->map[i * interval], champs[i].prog, champs[i].len_prog);
-		((t_process *)allprocess->content)->ptr = i * interval;
+		process = (t_process *)allprocess->content;
+		process->ptr = i * interval;
 		j = i * interval;
 		c = j;
 		while (j < c + champs[i].len_prog) {
 			map->c_map[j++] = champs[i].num + 1;
 		}
-		i++;
+		allprocess = allprocess->next;
 	}
 }
