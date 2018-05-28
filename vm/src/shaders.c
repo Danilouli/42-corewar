f58a4d75ee2934d4b52ee39a2cbeafa79929ef95
Fabien SABATIE <fsabatie@e2r1p13.42.fr>
2018-05-28 17:43:22 +0200

    Done

--- vm/src/shaders.c
+++ vm/src/shaders.c
@@ -12,7 +12,7 @@
 
 #include "corewar.h"
 
-static GLchar	*getFileSource(char *filename)
+static GLchar	*getfilesource(char *filename)
 {
 	char	*line;
 	char	*src;
@@ -30,19 +30,6 @@ static GLchar	*getFileSource(char *filename)
 	return ((GLchar*)src);
 }
 
-// static int		errShaderCompilation(t_shader *s, char *filename)
-// {
-// 	if (s->info.success != GL_TRUE)
-// 	{
-// 		glGetShaderiv(s->id, GL_INFO_LOG_LENGTH, &(s->info.msg_len));
-// 		glGetShaderInfoLog(s->id, LOG_BUFFSIZE, &(s->info.msg_len), s->info.log);
-// 		s->info.log[s->info.msg_len] = '\0';
-// 		ft_printf("Erreur de compilation pour \"%s\"\n%s", filename, s->info.log);
-// 		return (1);
-// 	}
-// 	return (0);
-// }
-
 t_shader		*build_shader(char *filename, GLenum type, GLuint prog_id, \
 				t_bool prog)
 {
@@ -52,13 +39,11 @@ t_shader		*build_shader(char *filename, GLenum type, GLuint prog_id, \
 		|| !(s->info.log = (GLchar*)malloc(sizeof(LOG_BUFFSIZE))))
 		return (0);
 	if (!(s->id = glCreateShader(type))
-		|| !(s->src = getFileSource(filename)))
+		|| !(s->src = getfilesource(filename)))
 		return (0);
 	glShaderSource(s->id, 1, (const char *const *)&(s->src), 0);
 	glCompileShader(s->id);
 	glGetShaderiv(s->id, GL_COMPILE_STATUS, &(s->info.success));
-	// if (errShaderCompilation(s, filename))
-	// 	return (NULL);
 	s->prog = (prog == TRUE) ? prog_id : glCreateProgram();
 	glAttachShader(s->prog, s->id);
 	glLinkProgram(s->prog);
